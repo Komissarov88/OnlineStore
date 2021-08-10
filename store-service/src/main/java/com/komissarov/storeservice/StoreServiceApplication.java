bc29d6d081b9277e4e461d9a324d6caac1402c4e
Anton Komissarov <komissarov1988@gmail.com>
2021-08-10 18:07:45 +0300

    modular store

--- store-service/src/main/java/com/komissarov/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/com/komissarov/storeservice/StoreServiceApplication.java
@@ -2,10 +2,8 @@ package com.komissarov.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
 public class StoreServiceApplication {
 
     public static void main(String[] args) {
